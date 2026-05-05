4a0128d092055eda2794a3e37dce3fb161893311
F S R <cursos.0171@gmail.com>
2026-05-05 17:33:38 -0300

    Termino da Classe Conta e ContaCorrente com verifição para ver se não ultrapassar o limites de saques(3) e o valor na conta

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ class PessoalFisica(Cliente):
         self.cpf = cpf
 
 class Conta:
+
         def __init__(self, numero , clinte) :
             self._saldo = 0
             self._numero = numero
@@ -47,10 +48,61 @@ class Conta:
         @property
         def historico(self):
             return self._historico
-        def sacar(self, valor)
-            
+        
+        def sacar(self, valor):
+            saldo = self.saldo
+            execedeu_saldo = valor > saldo
+
+            if execedeu_saldo:
+                print("\n--- Operação Falhou! Você não tem saldo suficiente.")
+            elif valor > 0:
+                self._saldo -=valor
+                print("\n--- Saque Realizado com sucesso!")
+                return True
+            else:
+                print("\n Operação falhou! o valor informado é invalido.")
+            return False
+        def depositar(self,valor):
+            if valor > 0:
+                self._saldo += valor
+                print("\n Depósito realizado com sucesso!")
+            else:
+                print("--- Operação falhou! o valor informado é inválido")
+                return False
+            return True
+
 
 class ContaCorrente(conta):
+    def __init__(self, numero, cliente, limite=500, limite_saques=3):
+        super().__init__(numero, cliente)
+        self.limite = limite
+        self.limite_saques = limite_saques
+
+    def sacar(self, valor):
+        numeros_saques = len(
+            [transacao for transacao in self.historico.transacoes if transacao["tipo"]== Saque.__name__]
+        )
+        execedeu_limite = valor > self.limite
+        execedeu_saques = numeros_saques > self.limite_saques
+
+        if execedeu_limite:
+            print("\n Operação falhou! o valor do saque excede o limite.")
+
+        elif execedeu_saques:
+            print("\n Operação falhou! Número máxímo de saques excedido.")
+
+        else:
+            return super().sacar(valor)
+        return False
+    
+        def __str__(self):
+            return f"""\
+            Agência:\t{self.agencia}
+            C/C:\t\t{self.numero}
+            Titular:\t{self.cliente.nome}
+            """
+
+# 0925
 
 class Historico:
 
